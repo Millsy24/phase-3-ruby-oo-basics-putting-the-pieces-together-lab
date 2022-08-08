1a725afa1f3dd34c25ccfc40b9af65bf7ed42425
Lukas Mills <lukascodes64@gmail.com>
2022-08-08 11:46:40 -0400

    commit tree ccf72e8ce018e3e8e57386da9aba7cb2aa9972b5

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,21 @@
+class Book
+    attr_accessor :title,:author, :page_count, :genre
 
+    def initialize(title)
+        @title = title
+    end
+    
+    def properties(author, page_count, genre)
+        @author = author
+        @page_count = page_count
+        @genre = genre
+
+    end
+    def turn_page
+        puts "Flipping the page...wow, you read fast!"
+    end
+
+end
+
+book = Book.new("And Then There Were None")
 
